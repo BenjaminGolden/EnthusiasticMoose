26bcb569e52959ec45ab27ef635545c3fded4c9b
Benjamin Golden <b.j.golden@outlook.com>
2021-05-21 15:20:50 -0500

    phase 9

--- Program.cs
+++ Program.cs
@@ -13,64 +13,14 @@ MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
 //ask a question
-CanadaQuestion();
-EnthusiasticQuestion();
-LoveCSharpQuestion();
-SecretQuestion();
+MooseQuestion("is Canada Real?", "Really? It seems very unlikely.", "I  K N E W  I T  !!!");
+MooseQuestion("Are you enthusiastic?", "Yay!", "You should try it!");
+MooseQuestion("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...");
+MooseQuestion("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
 }
 
-void CanadaQuestion()
-{
-bool isTrue = MooseAsks("Is Canada real?");
-if (isTrue)
-    {
-        MooseSays("Really? It seems very unlikely.");
-    }
-    else 
-    {
-        MooseSays("I  K N E W  I T  !!!");
-    }
-}
 
-void EnthusiasticQuestion()
-{
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
-    }
-}
-void LoveCSharpQuestion()
-{
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
-}
-
-void SecretQuestion()
-{
-    bool wantsSecret = MooseAsks("Do you want to know a secret?");
-    if (wantsSecret)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
-    }
-}
-
-bool MooseAsks(string question)
+void MooseQuestion(string question, string AnswerTrue, string AnswerFalse)
 {
     Console.Write($"{question} (Y/N): ");
     string answer = Console.ReadLine().ToLower();
@@ -83,13 +33,17 @@ bool MooseAsks(string question)
 
     if (answer == "y")
     {
-       return true;
+        
+       MooseSays(AnswerTrue);
     }
     else 
     {
-       return false;
+       MooseSays(AnswerFalse);
     }
 }
+
+
+
 
 
 void MooseSays(string message)
@@ -124,3 +78,24 @@ void MooseSays(string message)
     ");
 }
 
+
+// bool MooseAsks(string question)
+// {
+//     Console.Write($"{question} (Y/N): ");
+//     string answer = Console.ReadLine().ToLower();
+
+//     while (answer != "y" && answer != "n")
+//     {
+//         Console.Write($"{question} (Y/N): ");
+//         answer = Console.ReadLine().ToLower();
+//     }
+
+//     if (answer == "y")
+//     {
+//        return true;
+//     }
+//     else 
+//     {
+//        return false;
+//     }
+// }
